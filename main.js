288f8708f06d8c142f84aa0b4c761b0587a897e3
Yoonjae-Moon <yoonjaemoon20@gmail.com>
2024-03-09 16:28:54 +0900

    0309 4pm

---
+++ main.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let currentImageIndex = 0;
+    const images = ['photos/photo1.png', 'photos/photo2.png', 'photos/photo3.png', 'photos/photo4.png'];
+    const slideshowElement = document.getElementById('slideshow');
+  
+    function changeImage() {
+      // Reset opacity to 0 to start the fade-out effect
+      slideshowElement.style.opacity = 0.5;
+  
+      // Change the image after the opacity transition has finished
+      setTimeout(() => {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        slideshowElement.src = images[currentImageIndex];
+  
+        // Fade-in the new image
+        slideshowElement.style.opacity = 1;
+      }, 2000); // Match this timeout with your CSS transition duration
+    }
+  
+    // Initially, fade in the first image
+    window.onload = () => { slideshowElement.style.opacity = 1; };
+  
+    // Change the image every 4 seconds
+    setInterval(changeImage, 4000);
+  });
+  
